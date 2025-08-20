9c5fda41e7417be83d692cc8c101e28f65824e19
Akira Yamamoto <dev.monster.connect@gmail.com>
2025-08-20 15:29:51 -0400

    update: reachable roads

--- 2025-08-21/02/reachable_roads.py
+++ 2025-08-21/02/reachable_roads.py
@@ -4,7 +4,18 @@ city_number = int(input())
 for i in range(city_number):
     road_endpoint = int(input())
     r = int(input())
+    road = []
     for j in range(r):
         road = list(map(int, input().split(" ")))
-    for k in range(len(road)-1):
-        
+
+    useless_endpoint = []
+    for k in range(r):
+        total_count = 0
+        for val in enumerate(road):
+            if val[0] == k or val[1] == k:
+                total_count += 1
+        if total_count < 2:
+            useless_endpoint.append(k)
+    print(len(useless_endpoint) - 1)
+
+# This problem is not completed yet.
