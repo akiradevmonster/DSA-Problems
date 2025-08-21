bc65f8ff22c465779a2e80226a151e28383c6beb
DESKTOP-LJ4F1J1\Apple <dev.monster.connect@gmail.com>
2025-08-22 01:54:18 +0900

    update: reachable roads

--- 2025-08-20/02/reachable_roads.py
+++ 2025-08-20/02/reachable_roads.py
@@ -1,30 +1,36 @@
 """Kattis : Reachable Roads"""
 
-city_number = int(input())
 
-for i in range(city_number):
-    road_endpoint = int(input())
-    r = int(input())
-    road = []
-    for j in range(r):
-        roadtemp = list(map(int, input().split(" ")))
-        road.append([roadtemp[0], roadtemp[1]])
+def main():
+    """Main Function"""
+    city_number = int(input())
 
-    total_count = 0
-    for k in range(road_endpoint - 1):
-        useful = False
-        for val in road:
-            if (val[0] == k and val[1] == k + 1) or (val[1] == k and val[0] == k + 1):
-                useful = True
-        if not useful:
-            total_count += 1
-    # for k in range(r):
-    #     total_count = 0
-    #     for val in enumerate(road):
-    #         if val[0] == k or val[1] == k:
-    #             total_count += 1
-    #     if total_count < 2:
-    #         useless_endpoint.append(k)
-    print(total_count)
+    i = 0
+    while i < city_number:
+        road_endpoint = int(input())
+        r = int(input())
+        road = []
+        j = 0
+        while j < r:
+            roadtemp = list(map(int, input().split(" ")))
+            road.append([roadtemp[0], roadtemp[1]])
+            j += 1
 
-# This problem is completed now.
+        total_count = 0
+
+        # main search logic
+        for k in range(road_endpoint - 1):
+            useful = False
+            for val in road:
+                if (val[0] == k and val[1] == k + 1) or (
+                    val[1] == k and val[0] == k + 1
+                ):
+                    useful = True
+            if not useful:
+                total_count += 1
+        print(total_count)
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
