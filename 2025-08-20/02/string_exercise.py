2a2270401ed38a5e810f0b4ff328fa4fe93336fe
DESKTOP-LJ4F1J1\Apple <dev.monster.connect@gmail.com>
2025-08-22 01:53:39 +0900

    update: string exercise

--- 2025-08-20/02/string_exercise.py
+++ 2025-08-20/02/string_exercise.py
@@ -4,12 +4,9 @@ text = input()
 
 # Initialize lowertext
 resulttext = []
-lowertext = []
-voweltext = ["a", "o", "y", "e", "u", "i"]
 for s in text:
     c = s.lower()
-    if c not in voweltext:
-        lowertext.append(c)
+    if c not in ["a", "o", "y", "e", "u", "i"]:
         resulttext.append(c)
 
 print("." + "".join(resulttext))
