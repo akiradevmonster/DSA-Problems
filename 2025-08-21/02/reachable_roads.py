9f9764c8e8c3c7dd9cd42afa7441908e3324c0a4
Akira Yamamoto <dev.monster.connect@gmail.com>
2025-08-20 20:24:07 -0400

    complete: reachable roads

--- 2025-08-21/02/reachable_roads.py
+++ 2025-08-21/02/reachable_roads.py
@@ -1,21 +1,30 @@
 """Kattis : Reachable Roads"""
 
 city_number = int(input())
+
 for i in range(city_number):
     road_endpoint = int(input())
     r = int(input())
     road = []
     for j in range(r):
-        road = list(map(int, input().split(" ")))
+        roadtemp = list(map(int, input().split(" ")))
+        road.append([roadtemp[0], roadtemp[1]])
 
-    useless_endpoint = []
-    for k in range(r):
-        total_count = 0
-        for val in enumerate(road):
-            if val[0] == k or val[1] == k:
-                total_count += 1
-        if total_count < 2:
-            useless_endpoint.append(k)
-    print(len(useless_endpoint) - 1)
+    total_count = 0
+    for k in range(road_endpoint - 1):
+        useful = False
+        for val in road:
+            if (val[0] == k and val[1] == k + 1) or (val[1] == k and val[0] == k + 1):
+                useful = True
+        if not useful:
+            total_count += 1
+    # for k in range(r):
+    #     total_count = 0
+    #     for val in enumerate(road):
+    #         if val[0] == k or val[1] == k:
+    #             total_count += 1
+    #     if total_count < 2:
+    #         useless_endpoint.append(k)
+    print(total_count)
 
-# This problem is not completed yet.
+# This problem is completed now.
